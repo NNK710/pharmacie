92f9257eddc700fab84218d67438c35c4c9e10e6
VicMls38 <victor.morales@outlook.fr>
2021-10-18 17:16:58 +0200

    Modification index.js

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 // Exemple adapté de la mise en route d'Express : 
 // http://expressjs.com/fr/starter/hello-world.html
+const mysql = require('mysql')
 var express = require('express');
 var app = express();
+const bodyparser = require('body-parser')
+const iniparser = require('iniparser')
+
+
+let configDB = iniparser.parseSync('./DB.ini')
+let app = express()
+app.set('view engine', 'ejs')
+app.use(express.static('views'))
+app.use(express.static('public'))
+let mysqlconnexion = mysql.createConnection({
+ host:configDB['dev']['host'],
+ user:configDB['dev']['user'],
+ password:configDB['dev']['password'],
+ database:configDB['dev']['database']
+})
+mysqlconnexion.connect((err) => {
+ if (!err) console.log('BDD connectée.')
+ else console.log('BDD connexion échouée \n Erreur: '+JSON.stringify(err))
+})
 
 // '/' est la route racine
 app.get('/', function (req, res) {
