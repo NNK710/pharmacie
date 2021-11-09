b94854b777c97524f55973528358fe2b5b81005b
VicMls38 <victor.morales@outlook.fr>
2021-11-09 11:51:39 +0100

    Correction mineur code

--- controllers/SauteuhzController.js
+++ controllers/SauteuhzController.js
@@ -17,21 +17,21 @@ module.exports = {
                 });
         },
 
-        Clients_detail:function (req, res){
+        Clients_detail: (req, res) =>{
                 sauteuhzModel.Clients_detail(function(lignes){
                         console.log(lignes)
                         res.render("./detailClients", {index : lignes});
                 });
         },
 
-        Medocs_affichage:function (req, res){
+        Medocs_affichage: (req, res) =>{
                 sauteuhzModel.Medocs_affichage(function(lignes){
                         console.log(lignes)
                         res.render("./listeMedocs", {index : lignes});
                 });
         },
 
-        Medocs_detail:function (req, res){
+        Medocs_detail: (req, res) =>{
                 sauteuhzModel.Medocs_detail(function(lignes){
                         console.log(lignes)
                         res.render("./detailMedocs", {index : lignes});
